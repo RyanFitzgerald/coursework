d75701aba41b69b7386848aa7b544c3aec4f4cb9
Ryan Fitzgerald <rfitz042@uottawa.ca>
2017-08-13 13:28:04 -0400

    Finish modules

--- react-for-beginners/src/components/App.js
+++ react-for-beginners/src/components/App.js
@@ -16,8 +16,10 @@ class App extends React.Component {
     // Bind it so it can be used
     this.addFish = this.addFish.bind(this);
     this.updateFish = this.updateFish.bind(this);
+    this.removeFish = this.removeFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+    this.removeFromOrder = this.removeFromOrder.bind(this);
 
     // Initial states
     this.state = {
@@ -71,6 +73,12 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  removeFish(key) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = null; // Must delete this way for firebase
+    this.setState({ fishes });
+  }
+
   loadSamples() {
     this.setState({
       fishes: sampleFishes
@@ -88,6 +96,12 @@ class App extends React.Component {
     this.setState({ order });
   }
 
+  removeFromOrder(key) {
+    const order = {...this.state.order};
+    delete order[key]; // Can delete this way since this isnt hooked up to firebase
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -99,11 +113,15 @@ class App extends React.Component {
               }
             </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} params={this.props.params} />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} updateFish={this.updateFish} />
+        <Order fishes={this.state.fishes} order={this.state.order} params={this.props.params} removeFromOrder={this.removeFromOrder} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} updateFish={this.updateFish} removeFish={this.removeFish} storeId={this.props.params.storeId} />
       </div>
     );
   }
 }
+
+App.propTypes = {
+  params: React.PropTypes.object.isRequired,
+};
 
 export default App;
--- react-for-beginners/src/components/Inventory.js
+++ react-for-beginners/src/components/Inventory.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import AddFishForm from './AddFishForm';
+import base from '../base';
 
 class Inventory extends React.Component {
   constructor() {
     super();
     this.handleChange = this.handleChange.bind(this);
+    this.authenticate = this.authenticate.bind(this);
+    this.logout = this.logout.bind(this);
+    this.authHandler = this.authHandler.bind(this);
+    this.renderLogin = this.renderLogin.bind(this);
+    this.state = {
+      uid: null,
+      owner: null
+    };
+  }
+
+  componentDidMount() {
+    // Listen for authentications
+    base.onAuth((user) => {
+      if (user) {
+        this.authHandler(null, {user});
+      }
+    });
   }
 
   handleChange(e, key) {
@@ -18,8 +36,56 @@ class Inventory extends React.Component {
     this.props.updateFish(key, updatedFish);
   }
 
+  authenticate(provider) {
+    base.authWithOAuthPopup(provider, this.authHandler);
+  }
+
+  logout() {
+    base.unauth();
+    this.setState({ uid: null });
+  }
+
+  authHandler(err, authData) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // Grab store info
+    const storeRef = base.database().ref(this.props.storeId);
+
+    // Query firebsae once for the store data
+    storeRef.once('value', (snapshot) => {
+      const data = snapshot.val() || {};
+
+      // Claim as own if no owner
+      if (!data.owner) {
+        storeRef.set({
+          owner: authData.user.uid
+        });
+      }
+
+      this.setState({
+        uid: authData.user.uid,
+        owner: data.owner || authData.user.uid
+      });
+    });
+  }
+
+  renderLogin() {
+    return (
+      <nav className="login">
+        <h2>Inventory</h2>
+        <p>Sign in to manage your store's inventory</p>
+        <button className="github" onClick={() => this.authenticate('github')}>Login with GitHub</button>
+        <button className="facebook" onClick={() => this.authenticate('facebook')}>Login with Facebook</button>
+        <button className="twitter" onClick={() => this.authenticate('twitter')}>Login with Twitter</button>
+      </nav>
+    );
+  }
+
   renderInventory(key) {
-    const fish = this.props.fishes[key]
+    const fish = this.props.fishes[key];
     return (
       <div className="fish-edit" key={key}>
         <input onChange={(e) => this.handleChange(e, key)} type="text" name="name" placeholder="Name" value={fish.name} />
@@ -30,14 +96,34 @@ class Inventory extends React.Component {
         </select>
         <textarea onChange={(e) => this.handleChange(e, key)} type="text" name="desc" placeholder="Description" value={fish.desc}></textarea>
         <input onChange={(e) => this.handleChange(e, key)} type="text" name="image" placeholder="Image" value={fish.image} />
+        <button onClick={() => this.props.removeFish(key)}>Remove Fish</button>
       </div>
-    )
+    );
   }
 
   render() {
+    const logout = <button onClick={this.logout}>Log Out</button>;
+    // Check if not logged in
+    if (!this.state.uid) {
+      return (
+        <div>{this.renderLogin()}</div>
+      );
+    }
+
+    // Check if they are the owner
+    if (this.state.uid !== this.state.owner) {
+      return (
+        <div>
+          <p>Sorry, you aren't the owner of the store.</p>
+          {logout}
+        </div>
+      );
+    }
+
     return (
       <div>
           <h2>Inventory</h2>
+          {logout}
           {Object.keys(this.props.fishes).map(this.renderInventory.bind(this))}
           <AddFishForm addFish={this.props.addFish} />
           <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
@@ -45,5 +131,14 @@ class Inventory extends React.Component {
     );
   }
 }
+
+Inventory.propTypes = {
+  fishes: React.PropTypes.object.isRequired,
+  addFish: React.PropTypes.func.isRequired,
+  updateFish: React.PropTypes.func.isRequired,
+  removeFish: React.PropTypes.func.isRequired,
+  loadSamples: React.PropTypes.func.isRequired,
+  storeId: React.PropTypes.string.isRequired
+};
 
 export default Inventory;
