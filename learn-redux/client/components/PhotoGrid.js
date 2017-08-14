41363bf4966a4c302c3fd002f50810a849249794
Ryan Fitzgerald <rfitz042@uottawa.ca>
2017-08-14 18:16:56 -0400

    Learn redux initial commit

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PhotoGrid extends React.Component {
+  render() {
+    return (
+      <div className="photo-grid">
+        photo grid
+      </div>
+    );
+  }
+}
+
+export default PhotoGrid;
---
+++ learn-redux/client/reduxstagram.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+// Import CSS
+import css from './styles/style.styl';
+
+// Import components
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+// Import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Router path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Router>
+  </Router>
+);
+
+render(router, document.getElementById('root'));
