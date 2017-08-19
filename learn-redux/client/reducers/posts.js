8b5cccc35374bd6d8f8a2629931bd71ecb8d553e
Ryan Fitzgerald <rfitz042@uottawa.ca>
2017-08-18 21:24:33 -0400

    Finished to module 12

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -2,8 +2,17 @@
 
 // Post reducer
 function posts(state = [], action) { // Use defaults for state
-  console.log(state, action);
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES':
+      const i = action.index;
+      return [
+        ...state.slice(0,i), // Before updated one
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i+1) // After updated one
+      ];
+    default:
+      return state;
+  }
 }
 
 export default posts;
