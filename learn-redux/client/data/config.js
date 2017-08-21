6cc0bcc65268fbd42db0400bc38b83286f0b3643
Ryan Fitzgerald <rfitz042@uottawa.ca>
2017-08-20 21:24:51 -0400

    Finish all modules

--- learn-redux/client/data/config.js
+++ learn-redux/client/data/config.js
@@ -1,7 +1,7 @@
 import Raven from 'raven-js';
 
-const sentry_key = 'cb55d4f05cd443ce82303222f77ef5e0';
-const sentry_app = '61499';
+const sentry_key = 'e33ebd20073e41189245bb8d5fb99b48';
+const sentry_app = '206155';
 export const sentry_url = `https://${sentry_key}@app.getsentry.com/${sentry_app}`;
 
 export function logException(ex, context) {
--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -15,9 +15,22 @@ const defaultState = {
   comments
 }
 
+// Store enhancers for redux dev tools, etc
+const enhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+);
+
 // Create redux store
-const store = createStore(rootReducer, defaultState);
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
+
+// Hot reloading redux reducers
+if (module.hot) {
+  module.hot.accept('./reducers/', () => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
 
 export default store;
