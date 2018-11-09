7bfee0267e3e65c0d99273590aef6ae3341a211f
Ryan Fitzgerald <me@ryanfitzgerald.ca>
2018-11-09 01:19:11 -0500

    Add create user mutation + datamodel

--- advanced-react/backend/src/resolvers/Mutation.js
+++ advanced-react/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: check if logged in
@@ -35,6 +38,33 @@ const Mutations = {
 
     // Delete it (`info` comes from frontend)
     return ctx.db.mutation.deleteItem({where}, info);
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    
+    // Hash the password
+    const password = await bcrypt.hash(args.password, 10);
+
+    // Create the user in db
+    const user = await ctx.db.mutation.createUser({
+      data: {
+        ...args,
+        password, // overwrite password with hash
+        permissions: { set: ['USER'] },
+      }
+    }, info);
+
+    // Create JWT token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    // Set JWT as cookie on response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+
+    // Return user to the browser
+    return user;
   }
 };
 
